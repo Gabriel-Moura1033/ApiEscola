1cc99929ad4708c21fb9e2fd0c37c0df59603e4a
Gabriel Moura <rez2000@outlook.com>
2023-07-07 21:04:38 -0300

    tabela Pessoas completa

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -37,6 +37,50 @@ class PessoaController {
             return res.status(500).json(error.message);
         }
     }
+
+    static async atualizaPessoa(req,res) {
+        const novaInfo = req.body;
+        const { id } = req.params;
+
+        try {
+            await database.Pessoas.update(novaInfo, {
+                where: {
+                    id: Number(id)
+                }
+            })
+
+            // o método update não retorna os dados atualizados, portanto é necessário 
+            // fazer a busca pelo ID para retornar para o Front/usuário.
+            const pessoaAtualizada = await database.Pessoas.findOne( 
+                { 
+                    where: { 
+                        id: Number(id) 
+                    } 
+                } 
+                )
+            
+            return res.status(200).json(pessoaAtualizada);
+
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+
+    }
+    
+    static async deletePessoa (req, res) {
+        const { id } = req.params;
+
+        try {
+            await database.Pessoas.destroy( {
+                where: {
+                    id: Number(id)
+                }
+            } )
+            return res.status(200).json({ mensagem: `O registro do id ${id} foi deletado com sucesso.` })
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
 }
 
 module.exports = PessoaController;
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -7,7 +7,7 @@ router
     .get('/pessoas', PessoaController.pegaTodasPessoas)
     .get('/pessoas/:id', PessoaController.selecionaPessoa)
     .post('/pessoas', PessoaController.criaPessoa)
-
-
-
+    .put('/pessoas/:id', PessoaController.atualizaPessoa)
+    .delete('/pessoas/:id', PessoaController.deletePessoa)
+    
 module.exports = router;
