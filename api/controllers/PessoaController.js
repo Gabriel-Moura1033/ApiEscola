265e1928f23eddfa8445e7e8fb9807de103627db
Gabriel Moura <rez2000@outlook.com>
2023-07-12 12:10:57 -0300

    Escopo/associacao

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -1,4 +1,5 @@
 const database = require('../models');
+const Sequelize = require('sequelize');
 
 class PessoaController {
     static async selecionaPessoasAtivas(req, res) {
@@ -177,6 +178,60 @@ class PessoaController {
             return res.status(500).json(error.message);
         }
     }
+
+    static async selecionaMatriculaAluno (req, res) {
+        const { estudanteId } = req.params
+
+        try {
+            const pessoa = await database.Pessoas.findOne( { where: { id: Number(estudanteId) } } )
+            const matriculas = await pessoa.getAulasMatriculadas()
+            return res.status(200).json(matriculas)
+            
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    } 
+
+    static async selecionaMatriculaTurma (req, res) {
+        const { turmaId } = req.params
+
+        try {
+            const matriculas = await database.Matriculas
+                .findAndCountAll({
+                    where: {
+                        turma_id: Number(turmaId),
+                        status: 'confirmado'
+                    },
+                    limit: 20,
+                    order: [['estudante_id', 'desc']]
+                })
+            return res.status(200).json(matriculas);
+            
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    } 
+
+    static async selecionaTurmasLotadas (req, res) {
+        const lotacaoTurma = 2;
+
+        try {
+            const turmasLotadas = await database.Matriculas
+                .findAndCountAll({
+                    where : {
+                        status: 'confirmado'
+                    },
+                    attributes: ['turma_id'],
+                    group: ['turma_id'],
+                    having: Sequelize.literal(`count(turma_id) >= ${lotacaoTurma}`)
+                })
+            return res.status(200).json(turmasLotadas.count);
+            
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    } 
+
 
 
 }
